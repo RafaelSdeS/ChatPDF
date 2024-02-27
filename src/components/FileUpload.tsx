1f0805d27fa3359837c7c7c9d054d69b836e2f3c
Rafael <rafesilvadesouza@gmail.com>
2024-02-27 10:28:16 -0300

    add react query

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,26 +1,60 @@
 'use client'
 
 import { uploadToS3 } from '@/lib/s3'
-import { Inbox } from 'lucide-react'
+import { Inbox, Loader2 } from 'lucide-react'
 import { useDropzone } from 'react-dropzone'
+import axios from 'axios'
+import { useMutation } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+import { useState } from 'react'
 
 const FileUpload = () => {
+  const [uploading, setUploading] = useState(false)
+  const { mutate, isLoading } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string
+      file_name: string
+    }) => {
+      const response = await axios.post('/api/create-chat', {
+        file_key,
+        file_name,
+      })
+      return response.data
+    },
+  })
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { 'application/pdf': ['pdf'] },
     maxFiles: 1,
     onDrop: async acceptedFiles => {
       const file = acceptedFiles[0]
       if (file.size > 10 * 1024 * 1024) {
-        alert('Please upload a smaller file')
+        toast.error('File too large')
         return
       }
 
       try {
+        setUploading(true)
         const data = await uploadToS3(file)
-        console.log(data)
+        if (!data?.file_key || !data.file_name) {
+          toast.error('Something went wrong!')
+          return
+        }
+        mutate(data, {
+          onSuccess: data => {
+            toast.success(data.message)
+          },
+          onError: error => {
+            toast.error('Error creating chat')
+          },
+        })
       } catch (error) {
         console.log(error)
       }
+      setUploading(false)
     },
   })
 
@@ -33,10 +67,19 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps()} />
-        <>
-          <Inbox className="w-10 h-10 text-blue" />
-          <p className="mt-2 text-sm text-slate-400">Drop PDF here</p>
-        </>
+        {uploading || isLoading ? (
+          <>
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">
+              Spilling Tea to GPT...
+            </p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue" />
+            <p className="mt-2 text-sm text-slate-400">Drop PDF here</p>
+          </>
+        )}
       </div>
     </div>
   )
